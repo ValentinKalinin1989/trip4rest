69f9aa21c8417cda8195b20bd611e7d8caaa9de6
Valentin Kalinin <k-valentin-1989@yandex.ru>
2021-11-23 23:55:19 +0300

    Add Spring Eureka

--- trip-storage/src/main/java/com/senla/tripstorage/TripStorageApplication.java
+++ trip-storage/src/main/java/com/senla/tripstorage/TripStorageApplication.java
@@ -2,8 +2,10 @@ package com.senla.tripstorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TripStorageApplication {
 
 	public static void main(String[] args) {
